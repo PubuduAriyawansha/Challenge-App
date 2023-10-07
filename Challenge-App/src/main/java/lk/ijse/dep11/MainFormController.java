4aa1d29cf68a1d68553326a24f959f7f746f1d79
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-10-07 13:59:14 +0530

    SetUp UI

---
+++ Challenge-App/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -0,0 +1,31 @@
+package lk.ijse.dep11;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public AnchorPane root;
+    public TextField txtName;
+    public TextField txtID;
+    public TextField txtCard;
+    public Button btnSave;
+    public Button btnDelete;
+    public Button btnNew;
+    public Button btnLoadCsv;
+    public TableView tblStudent;
+
+    public void btnSaveONACtion(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnACtion(ActionEvent actionEvent) {
+    }
+
+    public void btnNewOnACtion(ActionEvent actionEvent) {
+    }
+
+    public void btnLoadCsvOnAction(ActionEvent actionEvent) {
+    }
+}
